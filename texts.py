97c7288298e659cdbab263e98a9976dee030af3f
Muhammadumar <vohidoov7@gmail.com>
2026-06-09 11:51:34 +0500

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,16 @@
+MESSAGES = {
+    'ru': {
+        'start': 'Здравствуйте! Выберите язык для общения:',
+        'choice': 'Как вы хотите подтвердить покупку?',
+        'order_num': 'Прислать номер заказа',
+        'screenshot': 'Прислать скриншот',
+        'thanks': 'Спасибо! Ваша заявка принята, ожидайте проверки.'
+    },
+    'uz': {
+        'start': 'Assalomu alaykum! Tilni tanlang:',
+        'choice': 'Xaridingizni qanday tasdiqlamoqchisiz?',
+        'order_num': 'Buyurtma raqamini yuborish',
+        'screenshot': 'Skrinshot yuborish',
+        'thanks': 'Rahmat! Soʻrovingiz qabul qilindi, tekshiruvni kuting.'
+    }
+}
